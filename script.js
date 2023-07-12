5afa67ec14d64a80cfbfaefd39340fba2191dbe8
MokatilDev <mokatilgfx@gmail.com>
2023-07-12 11:42:48 +0100

    Frist commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const questions = [
+    {question : "Which is largest animal in the world?",
+    anwsors : [
+        {anwsor : "Shark", correct: false},
+        {anwsor : "Blue Whale", correct: true},
+        {anwsor : "Elephant", correct: false},
+        {anwsor : "Giraffe", correct: false}
+        ]
+    },
+    {question : "Which is largest animal in the world?",
+    anwsors : [
+        {anwsor : "Shark", correct: false},
+        {anwsor : "Blue Whale", correct: true},
+        {anwsor : "Elephant", correct: false},
+        {anwsor : "Giraffe", correct: false}
+        ]
+    },
+    {question : "Which is largest animal in the world?",
+    anwsors : [
+        {anwsor : "Shark", correct: false},
+        {anwsor : "Blue Whale", correct: true},
+        {anwsor : "Elephant", correct: false},
+        {anwsor : "Giraffe", correct: false}
+        ]
+    },
+    {question : "Which is largest animal in the world?",
+    anwsors : [
+        {anwsor : "Shark", correct: false},
+        {anwsor : "Blue Whale", correct: true},
+        {anwsor : "Elephant", correct: false},
+        {anwsor : "Giraffe", correct: false}
+        ]
+    },
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Start__Quiz = document.getElementById("Start__Quiz")
+
+Start__Quiz.addEventListener("click",()=>{
+    Start__Quiz.style.display = "none"
+})
