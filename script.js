e8574d1414c163eb70433c1e8d8a4fa101e75933
MokatilDev <mokatilgfx@gmail.com>
2023-07-13 14:53:27 +0100

    Show correct Anwsor

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 const questionsArray = [
-    {question : "What's my username?",
-        anwsors : [
-        {anwsor : "Mokatil_Gfx", correct: false},
-        {anwsor : "Mokatil_Designer", correct: false},
-        {anwsor : "Mokatil_Dev", correct: true},
-        {anwsor : "Mokatil_Dzn", correct: false}
+    {
+        question: "What's my username?",
+        anwsors: [
+            { anwsor: "Mokatil_Gfx", correct: false },
+            { anwsor: "Mokatil_Designer", correct: false },
+            { anwsor: "Mokatil_Dev", correct: true },
+            { anwsor: "Mokatil_Dzn", correct: false }
         ]
     },
-    {question : "What's my name?",
-        anwsors : [
-        {anwsor : "Abderrahman", correct: true},
-        {anwsor : "Ali", correct: false},
-        {anwsor : "Mohamad", correct: false},
-        {anwsor : "Alex", correct: false}
+    {
+        question: "What's my name?",
+        anwsors: [
+            { anwsor: "Abderrahman", correct: true },
+            { anwsor: "Ali", correct: false },
+            { anwsor: "Mohamad", correct: false },
+            { anwsor: "Alex", correct: false }
         ]
     },
-    {question : "What's my age?",
-        anwsors : [
-        {anwsor : "19", correct: false},
-        {anwsor : "15", correct: true},
-        {anwsor : "22", correct: false},
-        {anwsor : "17", correct: false}
+    {
+        question: "What's my age?",
+        anwsors: [
+            { anwsor: "19", correct: false },
+            { anwsor: "15", correct: true },
+            { anwsor: "22", correct: false },
+            { anwsor: "17", correct: false }
         ]
     },
-    {question : "What's my country?",
-        anwsors : [
-        {anwsor : "Italy", correct: false},
-        {anwsor : "Algerie", correct: false},
-        {anwsor : "France", correct: false},
-        {anwsor : "Morocco", correct: true}
+    {
+        question: "What's my country?",
+        anwsors: [
+            { anwsor: "Italy", correct: false },
+            { anwsor: "Algerie", correct: false },
+            { anwsor: "France", correct: false },
+            { anwsor: "Morocco", correct: true }
         ]
     },
 
@@ -47,22 +51,22 @@ const ScoreNumber = document.querySelector(".Score span")
 
 
 btnNext.style.cursor = "not-allowed"
-btnNext.setAttribute("disabled","")
+btnNext.setAttribute("disabled", "")
 
 let IndexOfQuestion = 0
 let Score = 0
 
-function slesct(e){
+function slesct(e) {
     btnNext.style.cursor = "pointer"
     btnNext.removeAttribute("disabled")
-    if(e.target.dataset.correct == "true"){
+    if (e.target.dataset.correct == "true") {
         Score += 1
         const correct = ` ✔`
         e.target.innerHTML += correct
         e.target.classList.add("correct")
-        e.target.setAttribute("disabled","")
-        Anwsors.forEach(element =>{
-            element.addEventListener("mousemove",()=>{
+        e.target.setAttribute("disabled", "")
+        Anwsors.forEach(element => {
+            element.addEventListener("mousemove", () => {
                 element.style.cssText = `
                 ackground-color: var(--Bg-Button);
                 color: var(--Color-Button);
@@ -70,7 +74,7 @@ function slesct(e){
                 color: var(--Color-Button);
                 `
             })
-            element.addEventListener("mouseleave",()=>{
+            element.addEventListener("mouseleave", () => {
                 element.style.cssText = `
                 background-color: var(--Bg-Button);
                 color: var(--Color-Button);
@@ -84,21 +88,21 @@ function slesct(e){
             color: var(--Color-Button);
             `
             element.style.cursor = "not-allowed"
-            element.setAttribute("disabled","")
+            element.setAttribute("disabled", "")
         })
         e.target.style.cssText = `
             background-color: var(--Bg-correct);
             border: 1px solid var(--boder-correct);
             color: var(--Color-Normal);
-        `  
+        `
         e.target.style.cursor = "not-allowed"
-    }else if(e.target.dataset.correct == "false"){
+    } else if (e.target.dataset.correct == "false") {
         Score += 0
         const incorrect = ` ✖`
         e.target.innerHTML += incorrect
-        e.target.setAttribute("disabled","")
-        Anwsors.forEach(element =>{
-            element.addEventListener("mousemove",()=>{
+        e.target.setAttribute("disabled", "")
+        Anwsors.forEach(element => {
+            element.addEventListener("mousemove", () => {
                 element.style.cssText = `
                 ackground-color: var(--Bg-Button);
                 color: var(--Color-Button);
@@ -106,7 +110,7 @@ function slesct(e){
                 color: var(--Color-Button);
                 `
             })
-            element.addEventListener("mouseleave",()=>{
+            element.addEventListener("mouseleave", () => {
                 element.style.cssText = `
                 background-color: var(--Bg-Button);
                 color: var(--Color-Button);
@@ -120,21 +124,37 @@ function slesct(e){
             color: var(--Color-Button);
             `
             element.style.cursor = "not-allowed"
-            element.setAttribute("disabled","")
+            element.setAttribute("disabled", "")
+
+        })
+        
+        const correctAaswor = Array.from(e.target.parentElement.children)
+        correctAaswor.forEach(element => {
+            if (element.dataset.correct == "true") {
+                element.style.cssText = `
+                    background-color: var(--Bg-correct);
+                    border: 1px solid var(--boder-correct);
+                    color: var(--Color-Normal);
+                   
+                    `
+                element.textContent += ` ✔`
+                console.log(element);
+            }
+
         })
         e.target.style.cssText = `
             background-color: var(--Bg-incorrect);
             border: 1px solid var(--Boder-incorrect);
             color: var(--Color-Normal);
-        `  
-        
+        `
+
     }
 
 }
 
-function showQuestion(){
+function showQuestion() {
     let AnwsorsNumber = 0
-    if(IndexOfQuestion > questionsArray.length - 1){
+    if (IndexOfQuestion > questionsArray.length - 1) {
         main.classList.remove("active")
         Scor.classList.add("Active")
         ScoreNumber.textContent = Score
@@ -153,17 +173,17 @@ function showQuestion(){
     })
 }
 
-btnNext.addEventListener("click",()=>{
+btnNext.addEventListener("click", () => {
     btnNext.style.cursor = "not-allowed"
-    btnNext.setAttribute("disabled","")
+    btnNext.setAttribute("disabled", "")
     IndexOfQuestion++
     showQuestion()
-    if(IndexOfQuestion == questionsArray.length -1){
+    if (IndexOfQuestion == questionsArray.length - 1) {
         btnNext.textContent = "Done!"
-        
+
     }
-    Anwsors.forEach(element =>{
-        element.classList.remove("correct","incorrect")
+    Anwsors.forEach(element => {
+        element.classList.remove("correct", "incorrect")
         element.style.cssText = `
         background-color: var(--Bg-Button);
         color: var(--Color-Button);
@@ -172,14 +192,14 @@ btnNext.addEventListener("click",()=>{
         
         `
         element.removeAttribute("disabled")
-        element.addEventListener("mousemove",()=>{
+        element.addEventListener("mousemove", () => {
             element.style.cssText = `
             background-color: var(--Boder-Basic);
             border: 1px solid var(--Color-Basic);
             color: var(--Color-Normal);
             `
         })
-        element.addEventListener("mouseleave",()=>{
+        element.addEventListener("mouseleave", () => {
             element.style.cssText = `
             background-color: var(--Bg-Button);
             color: var(--Color-Button);
@@ -188,23 +208,23 @@ btnNext.addEventListener("click",()=>{
             `
         })
     })
-    
+
 })
 
 
-Start__Quiz.addEventListener("click",()=>{
+Start__Quiz.addEventListener("click", () => {
     main.classList.add("active")
     Start__Quiz.classList.add("active")
     showQuestion()
-    Anwsors.forEach(element =>{
-        element.addEventListener("mousemove",()=>{
+    Anwsors.forEach(element => {
+        element.addEventListener("mousemove", () => {
             element.style.cssText = `
             background-color: var(--Boder-Basic);
             border: 1px solid var(--Color-Basic);
             color: var(--Color-Normal);
             `
         })
-        element.addEventListener("mouseleave",()=>{
+        element.addEventListener("mouseleave", () => {
             element.style.cssText = `
             background-color: var(--Bg-Button);
             color: var(--Color-Button);
